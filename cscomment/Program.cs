8cdc85607c521cee3971db3d3172cc540fc9160d
Kang JooYoung <s2018s21@e-mirim.hs.kr>
2020-05-06 15:40:55 +0900

    초기 커밋.

---
+++ cscomment/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cscomment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            Console.WriteLine("--숫자를 입력해--");
+            Console.Write("x: ");
+            x = int.Parse(Console.ReadLine());//입력
+            Console.Write("y: ");
+            y = int.Parse(Console.ReadLine());//입력
+
+            if (x > 4)
+            {
+                if (y > 2)
+                {
+                    Console.WriteLine(x * y);
+                }
+            }
+            else
+            {
+                Console.WriteLine();
+            }
+        }
+    }
+}
